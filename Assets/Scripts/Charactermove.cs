0c2a49e2500a9b92a6a99c75ae20a89e5e387e1d
Refrax <henao.montesdf97@gmail.com>
2025-03-24 22:53:43 -0500

    2.3

--- Assets/Scripts/Charactermove.cs
+++ Assets/Scripts/Charactermove.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Charactermove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float velocidad;
+    private Rigidbody2D rigidbody; // Se mantiene el nombre "rigidbody"
+    private bool mirandoDerecha = true;
+
+    private void Start() // Se corrige la mayúscula en "Start"
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>(); // Se usa correctamente la variable
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ProcesarMovimiento();
+    }
+
+    void ProcesarMovimiento()
+    {
+        float inputMovimiento = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(inputMovimiento * velocidad, rigidbody.velocity.y);
+        GestionarOrientacion(inputMovimiento);
+    } 
+
+    void GestionarOrientacion(float inputMovimiento)
+    {
+        //si se cumple condicion
+        if( (mirandoDerecha == true && inputMovimiento < 0) || (mirandoDerecha == false && inputMovimiento > 0))
+        {
+
+          // ejecutar codigo volteado
+          mirandoDerecha = !mirandoDerecha;
+          transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+
     }
 }
